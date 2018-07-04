5017d0ea8e82ed953d5184d74b31c623faffd937
liquidz <liquidz.uo@gmail.com>
2018-07-04 17:29:20 +0900

    Add :deploy-repositories to project.clj

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/liquidz/merr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.238"]]
   :plugins [[lein-cljsbuild "1.1.7"]
@@ -14,6 +15,7 @@
   :aliases
   {"test-cljs" ["with-profile" "test" "doo" "rhino" "test" "once"]
    "test-all"  ["do" ["test"] ["test-cljs"]]}
+
   :profiles
   {:dev {:dependencies [[testdoc "0.1.0-SNAPSHOT"]]}
    :test {:dependencies [[org.mozilla/rhino "1.7.10"]]
