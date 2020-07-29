fbb80883251e54951ae25b14720cd3f1dbf87d13
liquidz <liquidz.uo@gmail.com>
2020-07-30 06:13:25 +0900

    Version 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject merr "0.3.0-SNAPSHOT"
+(defproject merr "0.3.0"
   :description "Minimal and good enough error handling library for Clojure/ClojureScript"
   :url "https://github.com/liquidz/merr"
   :license {:name "Eclipse Public License"
