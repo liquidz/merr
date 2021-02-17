d7a9c61332ca9d4eac6b309d3f399e332d50f8bf
liquidz <liquidz.uo@gmail.com>
2021-02-17 16:55:47 +0900

    Bump dependencies

--- project.clj
+++ project.clj
@@ -5,17 +5,17 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories [["releases" :clojars]]
 
-  :plugins [[lein-cloverage "1.2.1"]]
+  :plugins [[lein-cloverage "1.2.2"]]
 
   :profiles
-  {:1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
-   :1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}
-   :1.10.1 {:dependencies [[org.clojure/clojure "1.10.1"]]}
+  {:1.9 {:dependencies [[org.clojure/clojure "1.10.2"]]}
+   :1.10 {:dependencies [[org.clojure/clojure "1.10.2"]]}
+   :1.10.1 {:dependencies [[org.clojure/clojure "1.10.2"]]}
    :provided [:1.10.1 {:dependencies [[org.clojure/clojurescript "1.10.773"]]}]
-   :test {:dependencies [[lambdaisland/kaocha "1.0.700"]
+   :test {:dependencies [[lambdaisland/kaocha "1.0.732"]
                          [lambdaisland/kaocha-cljs "0.0-71"]
-                         [testdoc "1.4.0"]
-                         [orchestra "2020.09.18-1"]
+                         [testdoc "1.4.1"]
+                         [orchestra "2021.01.01-1"]
                          [org.clojure/test.check "1.1.0"]]}
    :dev [:test :1.10.1
          {:source-paths ["dev" "src"]
